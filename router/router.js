dc1a44f799860a4306d33636919bfa0be05d888e
juan bernal <ingleso.com@gmail.com>
2018-08-05 13:52:44 -0500

    save arezzo files

--- router/router.js
+++ router/router.js
@@ -12,7 +12,7 @@ module.exports =  {
            var  MongoClient = require('mongodb').MongoClient,
             assert = require('assert'),
             url = process.env.MONGODB_URI || "mongodb://localhost:27017",
-            dbName= 'transportadores_asociados',
+            dbName= 'heroku_ztd3js1d',
             ObjectID = require('mongodb').ObjectID;
 
 
