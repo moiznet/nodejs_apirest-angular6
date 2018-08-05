57f474f7f530b863682ca30220eb982630c87c5a
juan bernal <ingleso.com@gmail.com>
2018-08-05 11:35:46 -0500

    probar router

---
+++ router/router.js
@@ -0,0 +1,39 @@
+module.exports =  { 
+
+    end_points :  function(app)   {  
+
+       var controller = require('../controllers/controller.js');
+       //app.route('/').get(function(req, res) {   controller.listall(req, res);  });
+      
+       app.route('/api/conductores')
+        .get(function(req, res)    {    res.send("hola tu");  })
+        .post(function(req, res)   {     })
+        .put(function(req, res)   {     }); 
+
+
+        app.route('/api/vehiculos')
+        .get(function(req, res)    {   res.send("hola tu");   })
+        .post(function(req, res)   {     })
+        .put(function(req, res)   {      }); 
+
+        app.route('/api/usuarios')
+        .get(function(req, res)    { res.send("hola tu");  })
+        .post(function(req, res)   {     })
+        .put(function(req, res)   {      });
+
+
+        app.route('/api/logger')
+        .get(function(req, res)    {   res.send("hola tu");   });
+
+ 
+       app.route('/api/asignar')
+         .get(function(req, res)    {    res.send("hola tu");   })
+         .put(function(req, res)    {      });
+
+     }
+	
+  }
+
+
+
+
