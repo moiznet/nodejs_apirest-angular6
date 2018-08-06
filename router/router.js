eb543bf3c6cfad82083be307a9ebfb0bc2c0b7c2
juan bernal <ingleso.com@gmail.com>
2018-08-05 22:00:17 -0500

    save arezzo files

--- router/router.js
+++ router/router.js
@@ -2,76 +2,33 @@ module.exports =  {
 
     end_points :  function(app)   {  
 
-       //var controller = require('../controllers/controller.js');
+       var controller = require('../controllers/controller.js');
        app.route('/').get(function(req, res) {  res.send("animo");  });
       
        app.route('/api/conductores')
-        .get(function(req, res)    {      })
-        .post(function(req, res)   {   
-
-           var  MongoClient = require('mongodb').MongoClient,
-            assert = require('assert'),
-            url = process.env.MONGODB_URI || "mongodb://localhost:27017",
-            dbName= 'heroku_ztd3js1d',
-            ObjectID = require('mongodb').ObjectID;
-
-
-
-         MongoClient.connect(url,(err, client)  =>  {
-            assert.equal(null, err);
-            //global.debug_logger("Conectado al servidor",false); 
-
-            const db = client.db(dbName);
-      
-
-         
-
-             db.collection('conductores').insertOne({
-
-                rol: "test",
-                cid:  "test",
-                nombre:  "test",
-                t_doc:  "test",
-                n_doc:  "test",
-                rh:  "test",
-                asignado:  "test"
-
-            }, function(err, doc) {
-                 if (err) {
-                   handleError(res, err.message, "Failed to create new contact.");
-                 } else {
-                   res.status(201).json(doc.ops[0]);
-                 }
-              });
-
-   
-
-            client.close();
-        });  
-
-
-         })
-        .put(function(req, res)   {     }); 
+        .get(function(req, res)    {   controller.listall_conductores(req, res);   })
+        .post(function(req, res)   {   controller.crear_conductor(req, res);   })
+        .put(function(req, res)   {   controller.modificar_conductor(req, res);   }); 
 
 
         app.route('/api/vehiculos')
-        .get(function(req, res)    {      })
-        .post(function(req, res)   {     })
-        .put(function(req, res)   {      }); 
+        .get(function(req, res)    {   controller.listall_vehiculos(req, res);   })
+        .post(function(req, res)   {   controller.crear_vehiculo(req, res);   })
+        .put(function(req, res)   {   controller.modificar_vehiculo(req, res);   }); 
 
         app.route('/api/usuarios')
-        .get(function(req, res)    {    })
-        .post(function(req, res)   {    })
-        .put(function(req, res)   {     });
+        .get(function(req, res)    {   controller.listall_usuarios(req, res);   })
+        .post(function(req, res)   {   controller.crear_usuario(req, res);   })
+        .put(function(req, res)   {   controller.modificar_usuario(req, res);   });
 
 
         app.route('/api/logger')
-        .get(function(req, res)    {      });
+        .get(function(req, res)    {   controller.listall_log(req, res);   });
 
  
        app.route('/api/asignar')
          .get(function(req, res)    {       })
-         .put(function(req, res)    {    });
+         .put(function(req, res)    {   controller.asignar(req, res);   });
 
      }
 	
