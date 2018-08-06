a9e8f065ebebb8eeff312f13661ade7aaf843138
juan bernal <ingleso.com@gmail.com>
2018-08-05 22:05:11 -0500

    save arezzo files

--- model/db.js
+++ model/db.js
@@ -3,7 +3,7 @@ module.exports = {
 
   MongoClient : require('mongodb').MongoClient,
   assert : require('assert'),
-  url : pocess.env.MONGODB_URI || 'mongodb://localhost:27017',
+  url : process.env.MONGODB_URI || 'mongodb://localhost:27017',
   dbName: 'transportadores_asociados',
   mongodb: require('mongodb'),
   ObjectID : require('mongodb').ObjectID
