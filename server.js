183c1dd811c662b792645c451932eceb2d3199cd
juan bernal <ingleso.com@gmail.com>
2018-08-10 18:19:46 -0500

    test images load on ui

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var express = require("express"),
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
-var images = __dirname + "/img/";
+var images = __dirname + "/img/src";
 app.use(express.static('images'));
 //app.use(express.static(__dirname));
 
