139a7f34c1c8b36ce077ec28457689384ba04012
juan bernal <ingleso.com@gmail.com>
2018-08-13 17:52:31 -0500

    usurios area finish

--- router/router.js
+++ router/router.js
@@ -5,8 +5,8 @@ module.exports =  {
        var controller = require('../controllers/controller.js');
 
        app.use(function(req, res, next) {
-          res.header("Access-Control-Allow-Origin", "*");
-          res.header('Access-Control-Allow-Methods', 'DELETE,PUT');
+      
+          res.header('Access-Control-Allow-Methods', 'DELETE,PUT,*');
           
           res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
           next();
