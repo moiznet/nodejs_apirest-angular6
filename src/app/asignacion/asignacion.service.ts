430b011a743bef8b7f810bc91a832c8d3cc7b1d4
juan bernal <ingleso.com@gmail.com>
2018-08-13 14:58:02 -0500

    usurios area finish

---
+++ src/app/asignacion/asignacion.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class VehiculosService {
+
+
+    private contactsUrl = 'https://damp-oasis-44947.herokuapp.com/api/vehiculos';
+    public listVehiculos = {};
+
+    private conductoresUrl = 'https://damp-oasis-44947.herokuapp.com/api/conductores';
+    public listConductores ={};
+
+    private asignarUrl = 'https://damp-oasis-44947.herokuapp.com/api/asignar';
+    public listAsignar ={};
+
+    public newasignar;
+
+    
+    constructor (private http: Http) {}
+
+    // get("/api/vehiculos")
+    getContacts() {
+      return this.http.get(this.contactsUrl)
+                 .toPromise()
+                 .then((response) => {console.log(response); this.listVehiculos = JSON.parse(response["_body"]) ;   })
+                 .catch(this.handleError);
+    }
+
+    // get("/api/conductores")
+    getConductores() {
+      return this.http.get(this.conductoresUrl)
+                 .toPromise()
+                 .then((response) => {console.log(response); this.listConductores = JSON.parse(response["_body"]) ;   })
+                 .catch(this.handleError);
+    }
+
+
+        // get("/api/conductores")
+    getAsignacion(newasignar) {
+      return this.http.put(this.asignarUrl, newasignar)
+                 .toPromise()
+                 .then((response) => {console.log(response); this.listAsignar = JSON.parse(response["nModified"]) ;   })
+                 .catch(this.handleError);
+    }
+
+
+    private handleError (error: any) {
+      let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+      console.error(errMsg); // log to console instead
+    }
+}
---
+++ src/app/asignacion/asignar.ts
@@ -0,0 +1,9 @@
+export class asignar {
+
+
+constructor(
+	public nombre: string,
+    public vehiid: string,
+	public condid: string
+	){}
+}
