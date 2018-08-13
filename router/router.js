be7baaf99d294fd481362f783e457ad2e318071c
juan bernal <ingleso.com@gmail.com>
2018-08-13 18:01:20 -0500

    usurios area finish

--- router/router.js
+++ router/router.js
@@ -38,7 +38,7 @@ module.exports =  {
 
 
         app.route('/api/logger')
-        .get(function(req, res)    {  res.end('yes');  });
+        .get(function(req, res)    {  controller.listall_log(req, res);   });
 
  
        app.route('/api/asignar')
